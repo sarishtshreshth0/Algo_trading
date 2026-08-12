510c6d14eb7a2f01486f91e7fbcde0eec80d9516
sarishtshreshth0 <sarishtshreshth@gmail.com>
2026-08-12 13:14:22 +0530

    fix bug

--- spreadsheet.py
+++ spreadsheet.py
@@ -11,6 +11,6 @@ def make_csv(data):
     if os.path.exists(r'C:\Users\JIT DAS\trading\trading_data.csv'):
         old_df = pd.read_csv(r'C:\Users\JIT DAS\trading\trading_data.csv')
         new_data = pd.concat([old_df , new_data] , ignore_index= True)
-        new_data.drop_duplicates(inplace =True)
+        new_data.drop_duplicates(['Ticker' , 'Date'] ,keep='first', inplace =True)
     new_data.to_csv(r"C:\Users\JIT DAS\trading\trading_data.csv", index=False)
     return 'Check trading_data.csv'
