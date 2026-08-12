c004a69340cf433c1fdc67f5b637359c7d12e21d
sarishtshreshth0 <sarishtshreshth@gmail.com>
2026-08-12 10:47:57 +0530

    refine app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+
+from fundamentals import results
+from spreadsheet import make_csv
 import csv
 import os
+# pyrefly: ignore [missing-import]
 from flask import Flask, render_template
-from trading import WATCHLIST, check_signal, get_historical_signals
-
+from trading import *
+from spreadsheet import * 
 app = Flask(__name__)
 
 # Use absolute path or fallback to script directory
@@ -12,6 +16,7 @@ CSV_PATH = os.path.join(BASE_DIR, 'trading_data.csv')
 @app.route('/')
 def home():
     current = []
+    make_csv(finals)
     if os.path.exists(CSV_PATH):
         with open(CSV_PATH, mode='r', encoding='utf-8') as f:
             reader = csv.DictReader(f)
