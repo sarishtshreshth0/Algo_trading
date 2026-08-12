ebc6bd04c48d3897dd522e0bf8a126166934a3c6
sarishtshreshth0 <sarishtshreshth@gmail.com>
2026-08-12 18:00:02 +0530

    modify path

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 data = []
+path = 'trading_data.csv'
 def make_data(r):
     data.append(r)
 
@@ -8,9 +9,9 @@ def make_csv(data):
     if not data:
         return 'no data to save'
     new_data = pd.DataFrame(data)
-    if os.path.exists(r'C:\Users\JIT DAS\trading\trading_data.csv'):
-        old_df = pd.read_csv(r'C:\Users\JIT DAS\trading\trading_data.csv')
+    if os.path.exists(path):
+        old_df = pd.read_csv(path)
         new_data = pd.concat([old_df , new_data] , ignore_index= True)
         new_data.drop_duplicates(['Ticker' , 'Date'] ,keep='last', inplace =True)
-    new_data.to_csv(r"C:\Users\JIT DAS\trading\trading_data.csv", index=False)
+    new_data.to_csv(path, index=False)
     return 'Check trading_data.csv'
